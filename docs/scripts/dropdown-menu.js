498e3308f841fbcbc3274b571ddc42743e784fd0
Lynn Wolters <lynn.wolters27@gmail.com>
2024-02-09 23:35:26 +0100

    Added dropdown menu

--- docs/scripts/dropdown-menu.js
+++ docs/scripts/dropdown-menu.js
@@ -1,7 +1,7 @@
-const dropdownMenuButton = document.querySelector(".dropdown-menu-button")
-const dropdownMenu = document.querySelector(".dropdown-menu")
+const dropdownMenuButton = document.querySelector(".dropdown-menu-button");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
 dropdownMenuButton.addEventListener("click", () => {
-    dropdownMenu.classList.toggle("animate-dropdown-menu")
-})
+    dropdownMenu.classList.toggle("animate-dropdown-menu");
+});
 
