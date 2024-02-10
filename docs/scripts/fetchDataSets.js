d4bc15f002f1dcab12e7f20ff04600bd48d71759
Joppe Koops <112714380+joppekoops@users.noreply.github.com>
2024-02-10 12:35:56 +0100

    Add functions for fetching external data from Rest Countries API and Wikipedia API (#1)
    * Fetch country description from Wikipedia API
    
    Created a function in scripts/fetchDataSets.js for fetching the Wikipedia API. It has to do it two times, because we first need to find what the exact title for the page is before we can look up that page.
    The function returns the HTML of the description that wikipedia gives back.
    
    * Fetch country data from Rest Country API & combine data from both external APIs in to one object

--- docs/scripts/fetchDataSets.js
+++ docs/scripts/fetchDataSets.js
@@ -26,3 +26,86 @@ Promise.all([requestJop, requestJoppe, requestEefje])
     });
 
 // https://rapidapi.com/guides/fetch-data-multiple-apis-with-fetch
+
+
+//Fetch data from Wikipedia api & Rest countries API code by: Joppe Koops
+
+// Replace spaces with _ for wiki titles
+const spaceToUnderscore = (string) => string.replace(/\s+/g, '_');
+
+// Get country description from Wikipedia
+
+const getCountryWikiExtractHTML = async (country) => {
+
+    // Search Wikipedia for the country name and get the title of the first article
+    let wikiSearchResult = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=json&search=${country}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            return response.json();
+        });
+
+    let wikiTitle = spaceToUnderscore(wikiSearchResult[1][0]);
+
+    //Get the actual wiki extract
+    let wikiDescrition = await fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&exsentences=5&format=json&titles=${wikiTitle}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            return response.json();
+        })
+        .then((response) => {
+            //The api is made to get multiple article, so eventhough we are only getting one we need to search for the id and get it out of an array.
+            let pages = response.query.pages;
+            pageId = Object.keys(pages)[0];
+            return pages[pageId].extract;
+        });
+
+    return await wikiDescrition;
+};
+
+// https://www.youtube.com/watch?v=RPz75gcHj18
+// https://www.mediawiki.org/w/api.php?action=help
+
+
+// Get data about country from Rest Countries API: https://restcountries.com/
+const fetchRestcountriesApi = async (country) => {
+    let result = await fetch(`https://restcountries.com/v3.1/name/${country}?fullText=true`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            return response.json();
+        })
+
+    return await result;
+}
+
+// Combine the data from both external API's in one usable object
+const getCountryData = async (country) => {
+    // Promise.all executes both async functions at the same time
+    let countryData = await Promise.all([getCountryWikiExtractHTML(country), fetchRestcountriesApi(country)])
+        .then(([extractHTML, restcountriesData]) => {
+            return {
+                // Restructure the data to a usable object
+                country: restcountriesData[0].name.common,
+                description: extractHTML,
+                flagImageUrl: restcountriesData[0].flags.svg,
+                flagImageAlt: restcountriesData[0].flags.alt,
+                capital: restcountriesData[0].capital[0],
+                population: restcountriesData[0].population
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    console.log(countryData);
+};
+
+getCountryData('france');
