a6faf7dbb5aaf4e0cfc75697afcb52bf52bcc2b4
Jop <jopmolenaar@icloud.com>
2024-02-10 15:06:44 +0100

    Getting data when you are clicking a country

--- docs/scripts/selectCountry.js
+++ docs/scripts/selectCountry.js
@@ -2,12 +2,18 @@
  * @Author:             Jop Molenaar
  * @Date created:       10-02-2024
  * @Date updated:       10-02-2024
- * @Description:        get the datasets of the paths to know which country you are clicking
+ * @Description:        Get the datasets of the paths to know which country you are clicking, 
+ *                      get the data of that country, and open the side menu with all the data inside.
  */
 
 const svgCountries = document.querySelectorAll("main svg path");
 svgCountries.forEach((country) => {
-    country.addEventListener("click", () => {
-        console.log(country.dataset.country);
+    country.addEventListener("click", async () => {
+        openSideMenuWithData(await getCountryData(`${country.dataset.country}`))
     });
 });
+
+
+const openSideMenuWithData = (data) => {
+    console.log(data);
+}
